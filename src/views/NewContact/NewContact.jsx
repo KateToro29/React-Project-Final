da0c6b4ddcb47274dbd2f23a3994e624263202c1
Daniel Mauricio Gutierrez Vanegas <dm.gutierrez@globant.com>
2024-03-15 16:58:29 -0500

    Add NewContant form

--- src/views/NewContact/NewContact.jsx
+++ src/views/NewContact/NewContact.jsx
@@ -1,3 +1,98 @@
+import { useState } from "react";
+import axios from "axios";
+import "./Style.css";
+
 export const NewContact = () => {
-  return <div>NewContact</div>;
+  const [formData, setFormData] = useState({
+    FirstName: "",
+    LastName: "",
+    Email: "",
+    resolved: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("https://reqres.in/api/users", formData);
+      alert("Contacto creado exitosamente");
+
+      setFormData({
+        FirstName: "",
+        LastName: "",
+        description: "",
+        resolved: false,
+      });
+    } catch (error) {
+      console.error("Error al crear contacto:", error);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="containerCampos">
+          <input
+            type="text"
+            className="campos"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            minLength="6"
+            maxLength="18"
+            placeholder="First name"
+          />
+        </div>
+        <div className="containerCampos">
+          <input
+            type="text"
+            className="campos"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            minLength="6"
+            maxLength="18"
+            placeholder="Last name"
+          />
+        </div>
+        <div className="containerCampos">
+          <input
+            type="email"
+            className="campos"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            minLength="6"
+            maxLength="18"
+            placeholder="Email"
+          />
+        </div>
+        <div className="containerCampos">
+          <input
+            type="checkbox"
+            name="resolved"
+            checked={formData.resolved}
+            onChange={handleChange}
+            className="checkbox"
+          />
+          <label className="">Enable like Favorite</label>
+        </div>
+        <div className="containerCampos">
+          <button className="ButtonSubmit" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
